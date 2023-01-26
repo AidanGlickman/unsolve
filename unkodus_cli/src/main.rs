504ca7cf41474404b12d650c69f93c70780887f7
Aidan Glickman <aidan@aidanglickman.com>
2023-01-25 20:30:35 -0600

    add basic cli implementation, awaiting some other functionality

--- unkodus_cli/src/main.rs
+++ unkodus_cli/src/main.rs
@@ -2,15 +2,15 @@ use rand::Rng;
 use unkodus_lib::SudokuBoard;
 fn main() {
     println!("UnKODUS CLI...");
-    let boardSeed = std::env::args()
+    let board_seed = std::env::args()
         .nth(1)
         .unwrap_or("No seed given".to_string());
-    let boardSeed = boardSeed.parse::<u64>().unwrap_or({
+    let board_seed = board_seed.parse::<u64>().unwrap_or({
         println!("No seed given, generating random seed");
         let mut rng = rand::thread_rng();
         rng.gen()
     });
-    println!("Seed: {}", boardSeed);
+    println!("Seed: {}", board_seed);
 
-    let mut board = SudokuBoard::new(boardSeed);
+    let mut board = SudokuBoard::new(board_seed);
 }
