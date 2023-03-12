f9a742b2db1f1b7d562114a757a5f436cb5f5185
Aidan Glickman <aidan@aidanglickman.com>
2023-03-12 16:40:51 -0500

    Add Serviceworker script so Z3 can access SharedArrayBuffer (#17)
    * add files for serviceworker
    
    * add test

--- src/test/basic.test.tsx
+++ src/test/basic.test.tsx
@@ -1,5 +1,11 @@
 import { assert, expect, test } from 'vitest'
+import Sudoku from '../lib/sudoku';
 
 test('no test', () => {
     expect(true).toBe(true)
+})
+
+test("Z3 is able to access the SharedArrayBuffer", async () => {
+    const testPuzzle = new Sudoku(111);
+    expect(async () => { await testPuzzle.init() }).not.toThrow()
 })
