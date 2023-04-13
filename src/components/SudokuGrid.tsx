3487cf0b610e1e83b9e8c0c6d796513c5ff436f6
Adam Aaronson <54685433+adamaaronson@users.noreply.github.com>
2023-04-13 16:14:09 -0500

    Make web interactive (#24)

--- src/components/SudokuGrid.tsx
+++ src/components/SudokuGrid.tsx
@@ -1,25 +1,30 @@
-import { Arith } from 'z3-solver'
 import '../css/SudokuGrid.scss'
 import { BOARD_SIZE, BOX_SIZE } from '../lib/sudoku'
 
 interface Props {
     cells: string[][]
+    onClickCell: (row: number, col: number) => void
+    frozen: boolean
 }
 
 function isEndOfBox(cellIndex: number) {
     return cellIndex % BOX_SIZE == BOX_SIZE - 1 && cellIndex != BOARD_SIZE - 1
 }
 
-export function SudokuGrid({ cells, }: Props) {
+export function SudokuGrid({ cells, onClickCell, frozen }: Props) {
     return <div className="sudoku-grid">
         {cells.map((row, rowIndex) => 
             <div className="sudoku-row" key={rowIndex}>
                 {row.map((cell, colIndex) =>
-                    <div className={`sudoku-cell ${isEndOfBox(rowIndex) ? "bottom" : ""} ${isEndOfBox(colIndex) ? "right" : ""}`} key={colIndex}>
+                    <button
+                        disabled={frozen}
+                        className={`sudoku-cell ${isEndOfBox(rowIndex) ? "bottom" : ""} ${isEndOfBox(colIndex) ? "right" : ""}`}
+                        onClick={() => onClickCell(rowIndex, colIndex)}
+                        key={colIndex}>
                         <div className="sudoku-cell-contents">
                             {cell.toString()}
                         </div> 
-                    </div>
+                    </button>
                 )}
             </div>
         )}
