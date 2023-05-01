c857cc5810efa9c127a03f820a1a3840beb1dea5
Adam Aaronson <adamaaronson@gmail.com>
2023-04-30 19:37:12 -0500

    Keep track of original solution to fade numbers instead of erasing them

--- src/components/SudokuGrid.tsx
+++ src/components/SudokuGrid.tsx
@@ -3,6 +3,7 @@ import { BOARD_SIZE, BOX_SIZE } from '../lib/sudoku'
 
 interface Props {
     cells: string[][]
+    originalCells: string[][]
     onClickCell: (row: number, col: number) => void
     frozen: boolean
 }
@@ -11,7 +12,7 @@ function isEndOfBox(cellIndex: number) {
     return cellIndex % BOX_SIZE == BOX_SIZE - 1 && cellIndex != BOARD_SIZE - 1
 }
 
-export function SudokuGrid({ cells, onClickCell, frozen }: Props) {
+export function SudokuGrid({ cells, originalCells, onClickCell, frozen }: Props) {
     return <div className="sudoku-grid">
         {cells.map((row, rowIndex) => 
             <div className="sudoku-row" key={rowIndex}>
@@ -21,8 +22,8 @@ export function SudokuGrid({ cells, onClickCell, frozen }: Props) {
                         className={`sudoku-cell ${isEndOfBox(rowIndex) ? "bottom" : ""} ${isEndOfBox(colIndex) ? "right" : ""}`}
                         onClick={() => onClickCell(rowIndex, colIndex)}
                         key={colIndex}>
-                        <div className="sudoku-cell-contents">
-                            {cell.toString()}
+                        <div className={`sudoku-cell-contents ${cell ? "" : "faded"}`}>
+                            {cell ? cell : originalCells[rowIndex][colIndex]}
                         </div> 
                     </button>
                 )}
